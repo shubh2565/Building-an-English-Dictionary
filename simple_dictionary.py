a22c9da29ab0ff97f3de60518ec1745e665f5609
shubh2565 <shubh2565@users.noreply.github.com>
2018-01-03 11:31:14 +0530

    Update simple_dictionary.py

--- simple_dictionary.py
+++ simple_dictionary.py
@@ -7,6 +7,7 @@ data = json.load(open('data.json'))
 
 def find_meaning(word):
     
+    word = word.lower()     # to covert the entered word into lower case, otherwise it will give an error
     return data[word]
 
 
