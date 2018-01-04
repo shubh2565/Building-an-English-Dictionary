eae5446e9e699287c0dadeb5fce49be227df9dc6
shubh2565 <shubh2565@users.noreply.github.com>
2018-01-04 08:02:41 +0530

    Update english_dictionary_updated.py

--- english_dictionary_updated.py
+++ english_dictionary_updated.py
@@ -13,11 +13,11 @@ def find_meaning(w):
 	elif w.title() in data:
 		return data[w.title()]
 
-	elif w.upper() in data:
+	elif w.upper() in data:              # checks for acronyms
 		return data[w.upper()]
 
-	elif len(get_close_matches(w, data.keys(), cutoff=0.7)) > 0:
-		yn = input("Did you mean %s instead? " % get_close_matches(w, data.keys())[0])
+	elif len(get_close_matches(w, data.keys(), cutoff=0.7)) > 0:    # cutoff value can be changed to get the desired accuracy
+		yn = input("Did you mean %s instead? " % get_close_matches(w, data.keys())[0])  # [0] is used to give the best match (by default n=3)
 		if yn is 'y':
 			return data[get_close_matches(w, data.keys())[0]]
 		else:
@@ -26,7 +26,7 @@ def find_meaning(w):
 	else:
 		return "This word does't exist"
 
-
+# to enter as many times as the user desires
 choice = 'y'
 
 while(choice is 'y'):
@@ -34,7 +34,7 @@ while(choice is 'y'):
     word = input('Enter a word : ')
     output = (find_meaning(word))
 
-    if type(output) is list:
+    if type(output) is list:     # if more than one definition is present
     	for item in output:
     		print(item)
 
